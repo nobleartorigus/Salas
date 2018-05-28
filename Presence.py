32e2a2aa456321112d8e82dbba10a4b72b160467
nobleartorigus <danielfmf-caz@hotmail.com>
2018-05-28 17:18:50 -0500

    Actual status

--- Presence.py
+++ Presence.py
@@ -2,13 +2,31 @@ from SensorPIR import SensorPIR
 from Camera import Camerashoot
 import time
 
+
 x, y = SensorPIR()
 print(x, y)
-if x == 1:
-        Camerashoot()
+secs = 0
+min = 0
+#ID de sala 1, pendiente sistema para identificar IDs de salas diversas 
+id = 1
+while x == 1:
+	x, y = SensorPIR()
+	if secs != 10:
+		time.sleep(1)
+        	secs += 1
+	time.sleep(60)
+	Camerashoot()
+	#Despues de 60 s se suma un minuto al contador y este toma una captura cada minuto mientras permanezca en alto
+	min += 1 
 
+min = str(min)
+id = str(id)
+print('\n' + min)
 x = str(x)
 y = str(y)
 f = open('/home/chicago/Salas/Registro.txt','a')
+f1 = open('/home/chicago/Salas/UsoSalas.csv', 'a')
 f.write('\n' + x + '\n' + y)
 f.close()
+f1.write('\n' + id + '\t' + t)
+f1.close()
