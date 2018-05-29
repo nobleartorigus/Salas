0c49d7cb197c4906c8508a55463d9e9513beaac9
nobleartorigus <danielfmf-caz@hotmail.com>
2018-05-29 16:26:14 -0500

    Change variables name

--- timeProbing.py
+++ timeProbing.py
@@ -2,27 +2,30 @@
 import RPi.GPIO as GPIO
 import time
 
-GPIO.cleanup()
-GPIO.setmode(GPIO.BOARD)
+GPIO.cleanup()           #Clean ports
+GPIO.setmode(GPIO.BOARD) #Physical pin counter
 
-pir_in = 7
-led_out = 11
+sensor_pir_input = 7
+led_pir_indicator = 11
 
-GPIO.setup(pir_in,GPIO.IN)
-GPIO.setup(led_out,GPIO.OUT)
+time_initiate_counting = 0
+time_total_counted = 0
+
+GPIO.setup(sensor_pir_input,GPIO.IN)
+GPIO.setup(led_pir_indicator,GPIO.OUT)
 
 try:
   while True:
-    x = GPIO.input(pir_in)
+    x = GPIO.input(sensor_pir_input)
 
     if x == 1:
-      GPIO.output(led_out,True)
+      GPIO.output(led_pir_indicator,1)
     else:
-      GPIO.output(led_out,False)
+      GPIO.output(led_pir_indicator,0)
     #y = time.strftime("%c")
     print x
-    time.sleep(5)
+    time.sleep(0.2)
 
 except KeyboardInterrupt:
-  GPIo.cleanup()
+  GPIO.cleanup()
 
